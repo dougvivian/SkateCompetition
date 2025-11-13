fc855a6c2d82aa59f45e88ba82d7b49315a508a3
André Araujo deMenezes <57503927+aaamenezes@users.noreply.github.com>
2025-11-12 22:02:39 -0300

    Adicionar enum e propriedades

--- Obstaculo.java
+++ Obstaculo.java
@@ -1,3 +1,13 @@
+enum ObstaculoNome {
+    ESCADA,
+    CORRIMAO,
+    RAMP,
+    MESA,
+    HIDRANTE
+}
+
 public class Obstaculo {
-    
+    ObstaculoNome nome;
+    double pontos;
+    double dificuldade;
 }
