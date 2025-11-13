657e5baa56a79667958952911a08fea5dec11579
fabiovivian <fabio.vivian@icloud.com>
2025-11-12 22:25:34 -0300

    Create Etapa.java

---
+++ Etapa.java
@@ -0,0 +1,25 @@
+enum EtapaNome{
+    Linha,
+    Obstaculo,  
+}
+public class Etapa{
+    EtapaNome nome;
+    Obstaculo[] obstaculos;
+    
+    public Etapa(EtapaNome nome, Obstaculo[] obstaculos){
+    this.nome = EtapaNome;
+    }
+    public Etapanome getNome(){
+    return this.Etapanome;
+    }
+    public Obstaculo[] getObstaculo(){
+    return this.Obstaculo[];
+    }
+    public void setNome(EtapaNome nome){
+    
+    }
+    public void setObstaculo[](obstaculos){
+}
+
+    
+}
