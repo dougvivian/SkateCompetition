a271e2b646691263777d8691aa45e2690844f179
André Araujo deMenezes <57503927+aaamenezes@users.noreply.github.com>
2025-11-12 22:10:11 -0300

    Finalizar classe obstaculo

--- Obstaculo.java
+++ Obstaculo.java
@@ -10,4 +10,34 @@ public class Obstaculo {
     ObstaculoNome nome;
     double pontos;
     double dificuldade;
+    
+    public Obstaculo(ObstaculoNome nome, double pontos, double dificuldade) {
+        this.nome = nome;
+        this.pontos = pontos;
+        this.dificuldade = dificuldade;
+    }
+    
+    public ObstaculoNome getNome() {
+        return this.nome;
+    }
+    
+    public double getPontos() {
+        return this.pontos;
+    }
+    
+    public double getDificuldade() {
+        return this.dificuldade;
+    }
+    
+    public void setNome(ObstaculoNome nome) {
+        this.nome = nome;
+    }
+    
+    public void setPontos(double pontos) {
+        this.pontos = pontos;
+    }
+    
+    public void setDificuldade(double dificuldade) {
+        this.dificuldade = dificuldade;
+    }
 }
